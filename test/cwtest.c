ba8aa9530bcddb880eca41a0006002eb4f3729b3
Kamil Ignacak <acerion@users.sourceforge.net>
2012-06-17 10:50:05 +0200

    Reformatting + minimal refactoring.

--- test/cwtest.c
+++ test/cwtest.c
@@ -57,13 +57,11 @@
 #define K_SDEVICE 15	// set sound device
 #define K_VOLUME 16     // volume for soundcard
 
-int netkeyer_port = 6789;
-char netkeyer_hostaddress[16] = "127.0.0.1";
-int socket_descriptor;
-int close_rc;
-ssize_t sendto_rc;
+static int netkeyer_port = 6789;
+static char netkeyer_hostaddress[16] = "127.0.0.1";
+static int socket_descriptor;
 struct sockaddr_in address;
-struct hostent *hostbyname;
+
 
 int netkeyer_init(void);
 int netkeyer_close(void);
@@ -72,251 +70,265 @@ void catchint(int signal);
 
 
 
-int netkeyer_init (void)
+
+
+int netkeyer_init(void)
 {
-	hostbyname = gethostbyname (netkeyer_hostaddress);
-	if (hostbyname == NULL)
-	{
-		perror ("gethostbyname failed");
-		return (1);
+	struct hostent *host = gethostbyname(netkeyer_hostaddress);
+	if (!host) {
+		perror("gethostbyname() failed");
+		return -1;
 	}
+
 	memset(&address, '\0', sizeof(address));
 	address.sin_family = AF_INET;
-	memcpy (&address.sin_addr.s_addr, hostbyname->h_addr_list[0],
+	memcpy(&address.sin_addr.s_addr, host->h_addr_list[0],
 		sizeof (address.sin_addr.s_addr));
-	address.sin_port = htons (netkeyer_port);
-	socket_descriptor = socket (AF_INET, SOCK_DGRAM, 0);
-	if (socket_descriptor == -1)
-	{
-		perror ("socket call failed");
-		return (1);
+	address.sin_port = htons(netkeyer_port);
+	socket_descriptor = socket(AF_INET, SOCK_DGRAM, 0);
+	if (socket_descriptor == -1) {
+		perror("socket call failed");
+		return -1;
 	}
-	return (0);
+	return 0;
 }
 
-int netkeyer_close (void)
+
+
+
+
+int netkeyer_close(void)
 {
-
-	close_rc = close (socket_descriptor);
-	if (close_rc == -1)
-	{
-		perror ("close call failed");
-		return (-1);
+	if (close(socket_descriptor) == -1) {
+		perror("close call failed");
+		return -1;
 	}
-	return (0);
+
+	return 0;
 }
+
+
+
 
 
 int netkeyer(int cw_op, const char *cwmessage)
 {
 	char buf[80];
 
-	switch (cw_op)
-	{
+	switch (cw_op) {
 		case K_RESET :
-  			buf[0]= 27;
-			sprintf (buf+1,"0");
+  			buf[0] = 27;
+			sprintf(buf + 1, "0");
  			break;
 		case K_MESSAGE :
-			sprintf (buf, cwmessage);
+			sprintf(buf, cwmessage);
  			break;
 		case K_SPEED :
-  			buf[0]= 27;
-			sprintf (buf+1,"2");
-			sprintf (buf+2, cwmessage);
+  			buf[0] = 27;
+			sprintf(buf + 1, "2");
+			sprintf(buf + 2, cwmessage);
  			break;
 		case K_TONE :
-  			buf[0]= 27;
-			sprintf (buf+1,"3");
-			sprintf (buf+2, cwmessage);
+  			buf[0] = 27;
+			sprintf(buf + 1, "3");
+			sprintf(buf + 2, cwmessage);
  			break;
 		case K_ABORT :
-  			buf[0]= 27;
-			sprintf (buf+1,"4");
+  			buf[0] = 27;
+			sprintf(buf + 1, "4");
  			break;
 		case K_STOP :
-  			buf[0]= 27;
-			sprintf (buf+1,"5");
+  			buf[0] = 27;
+			sprintf(buf + 1, "5");
  			break;
 		case K_WORDMODE :
-  			buf[0]= 27;
-			sprintf (buf+1,"6");
+  			buf[0] = 27;
+			sprintf(buf + 1, "6");
  			break;
 		case K_WEIGHT :
-  			buf[0]= 27;
-			sprintf (buf+1,"7");
-			sprintf (buf+2, cwmessage);
+  			buf[0] = 27;
+			sprintf(buf + 1, "7");
+			sprintf(buf + 2, cwmessage);
  			break;
 		case K_DEVICE :
-  			buf[0]= 27;
-			sprintf (buf+1,"8");
-			sprintf (buf+2, cwmessage);
+  			buf[0] = 27;
+			sprintf(buf + 1, "8");
+			sprintf(buf + 2, cwmessage);
  			break;
 		case K_PTT :
-  			buf[0]= 27;
-			sprintf (buf+1,"a");
-			sprintf (buf+2, cwmessage);
+  			buf[0] = 27;
+			sprintf(buf + 1, "a");
+			sprintf(buf + 2, cwmessage);
  			break;
 		case K_TUNE :
-  			buf[0]= 27;
-			sprintf (buf+1,"c");
-			sprintf (buf+2, cwmessage);
+  			buf[0] = 27;
+			sprintf(buf + 1, "c");
+			sprintf(buf + 2, cwmessage);
  			break;
 		case K_TOD :
-  			buf[0]= 27;
-			sprintf (buf+1,"d");
-			sprintf (buf+2, cwmessage);
+  			buf[0] = 27;
+			sprintf(buf + 1, "d");
+			sprintf(buf + 2, cwmessage);
  			break;
 		case K_SDEVICE :
-  			buf[0]= 27;
-			sprintf (buf+1,"f");
-			sprintf (buf+2, cwmessage);
+  			buf[0] = 27;
+			sprintf(buf + 1, "f");
+			sprintf(buf + 2, cwmessage);
  			break;
 		case K_VOLUME :
-  			buf[0]= 27;
-			sprintf (buf+1,"g");
-			sprintf (buf+2, cwmessage);
+  			buf[0] = 27;
+			sprintf(buf + 1, "g");
+			sprintf(buf + 2, cwmessage);
  			break;
 		default :
-			buf[0]='\0';
+			buf[0] = '\0';
 	}
 
-	if (buf[0] != '\0')
-	{
-		sendto_rc = sendto (socket_descriptor, buf, sizeof (buf),
-			0, (struct sockaddr *)&address, sizeof (address));
+	ssize_t sendto_rc;
+	if (buf[0] != '\0') {
+		sendto_rc = sendto(socket_descriptor, buf, sizeof (buf),
+				   0, (struct sockaddr *)&address, sizeof (address));
 	}
 
 	buf[0] = '\0';
 	cw_op = K_RESET;
 
-	if (sendto_rc == -1)
-    	{
-     	 	printf ("Keyer send failed...!\n");
-		return (1);
-     	}
-	return(0);
+	if (sendto_rc == -1) {
+     	 	printf("Keyer send failed...!\n");
+		return -1;
+     	} else {
+		return 0;
+	}
 }
+
+
+
+
 
 void catchint(__attribute__((unused)) int signal)
 {
 	__attribute__((unused)) int result = netkeyer(K_ABORT, "");
-	exit (0);
+	exit(EXIT_SUCCESS);
 }
 
-int main (int argc, char **argv)
+
+
+
+
+int main(int argc, char **argv)
 {
+	if (netkeyer_init() == -1) {
+		exit(EXIT_FAILURE);
+	}
+
+	/* tests start here, no error handling */
 	int result;
-
-	result = netkeyer_init ();
-	if (result == 1) exit (1);
-
-/* tests start here, no error handling */
-	if (argc > 1)
-	{
-		result = netkeyer (K_DEVICE, argv[1]);
+	if (argc > 1) {
+		result = netkeyer(K_DEVICE, argv[1]);
 		printf("opening port %s\n", argv[1]);
 	}
 
 	printf("first message at initial speed\n");
-	result = netkeyer (K_MESSAGE, "paris");
-	sleep (3);
+	result = netkeyer(K_MESSAGE, "paris");
+	sleep(3);
 
 	printf("speed 40\n");
-	result = netkeyer (K_SPEED, "40");
-	result = netkeyer (K_MESSAGE, "paris");
-	sleep (2);
+	result = netkeyer(K_SPEED, "40");
+	result = netkeyer(K_MESSAGE, "paris");
+	sleep(2);
 
 	printf("tone 1000, speed 40\n");
-	result = netkeyer (K_TONE, "1000");
-	result = netkeyer (K_SPEED, "40");
-	result = netkeyer (K_MESSAGE, "paris");
-	sleep (2);
+	result = netkeyer(K_TONE, "1000");
+	result = netkeyer(K_SPEED, "40");
+	result = netkeyer(K_MESSAGE, "paris");
+	sleep(2);
 
 	printf("tone 800, weight +20\n");
-	result = netkeyer (K_TONE, "800");
-	result = netkeyer (K_WEIGHT, "20");
-	result = netkeyer (K_MESSAGE, "paris");
-	sleep (2);
+	result = netkeyer(K_TONE, "800");
+	result = netkeyer(K_WEIGHT, "20");
+	result = netkeyer(K_MESSAGE, "paris");
+	sleep(2);
 
 	printf("weight -20\n");
-	result = netkeyer (K_WEIGHT, "-20");
-	result = netkeyer (K_MESSAGE, "paris");
-	sleep (2);
+	result = netkeyer(K_WEIGHT, "-20");
+	result = netkeyer(K_MESSAGE, "paris");
+	sleep(2);
 
 	printf("weight 0\n");
-	result = netkeyer (K_WEIGHT, "0");
+	result = netkeyer(K_WEIGHT, "0");
 	printf("speed increase / decrease\n");
-	result = netkeyer (K_MESSAGE, "p++++++++++aris----------");
-	sleep (2);
+	result = netkeyer(K_MESSAGE, "p++++++++++aris----------");
+	sleep(2);
 
 	printf("half gap\n");
-	result = netkeyer (K_MESSAGE, "p~ari~s");
-	sleep (2);
+	result = netkeyer(K_MESSAGE, "p~ari~s");
+	sleep(2);
 
 	printf("tune 3 seconds\n");
-	result = netkeyer (K_TUNE, "3");
-	sleep (4);
+	result = netkeyer(K_TUNE, "3");
+	sleep(4);
 
 	printf("test message abort\n");
-	result = netkeyer (K_MESSAGE, "paris paris");
-	sleep (1.2);
-	result = netkeyer (K_ABORT, "");
-	sleep (1);
+	result = netkeyer(K_MESSAGE, "paris paris");
+	sleep(1.2);
+	result = netkeyer(K_ABORT, "");
+	sleep(1);
 
 	printf("switch to soundcard\n");
-	result = netkeyer (K_SDEVICE, "s");
-	result = netkeyer (K_MESSAGE, "paris");
-	sleep (2);
+	result = netkeyer(K_SDEVICE, "s");
+	result = netkeyer(K_MESSAGE, "paris");
+	sleep(2);
 
 	printf("volume 30\n");
-	result = netkeyer (K_VOLUME, "30");
-	result = netkeyer (K_MESSAGE, "paris");
-	sleep (2);
+	result = netkeyer(K_VOLUME, "30");
+	result = netkeyer(K_MESSAGE, "paris");
+	sleep(2);
 
 	printf("prosigns: SK BK SN AS AR\n");
-	result = netkeyer (K_MESSAGE, "< > ! & *");
-	sleep (4);
+	result = netkeyer(K_MESSAGE, "< > ! & *");
+	sleep(4);
 
 	printf("set volume back to 70\n");
-	result = netkeyer (K_VOLUME, "70");
-	result = netkeyer (K_MESSAGE, "paris");
-	sleep (2);
+	result = netkeyer(K_VOLUME, "70");
+	result = netkeyer(K_MESSAGE, "paris");
+	sleep(2);
 
 	printf("back to console\n");
-	result = netkeyer (K_SDEVICE, "c");
-	result = netkeyer (K_MESSAGE, "paris");
-	sleep (2);
+	result = netkeyer(K_SDEVICE, "c");
+	result = netkeyer(K_MESSAGE, "paris");
+	sleep(2);
 
 	printf("message with PTT on\n");
-	result = netkeyer (K_PTT, "1");
-	result = netkeyer (K_MESSAGE, "paris");
-	sleep (2);
-	result = netkeyer (K_PTT, "");
+	result = netkeyer(K_PTT, "1");
+	result = netkeyer(K_MESSAGE, "paris");
+	sleep(2);
+	result = netkeyer(K_PTT, "");
 
 	printf("same with different TOD\n");
-	result = netkeyer (K_TOD, "20");
-	result = netkeyer (K_PTT, "1");
-	result = netkeyer (K_MESSAGE, "paris");
-	sleep (2);
-	result = netkeyer (K_PTT, "");
-	result = netkeyer (K_TOD, "0");
+	result = netkeyer(K_TOD, "20");
+	result = netkeyer(K_PTT, "1");
+	result = netkeyer(K_MESSAGE, "paris");
+	sleep(2);
+	result = netkeyer(K_PTT, "");
+	result = netkeyer(K_TOD, "0");
 
 	/* almost done, reset keyer */
-	printf("done, reset\n");
-	result = netkeyer (K_RESET, "");
+	printf("almost done, reset\n");
+	result = netkeyer(K_RESET, "");
 
 
 	printf("test message abort with SIGALRM");
-	signal (SIGALRM, catchint);
-	result = netkeyer (K_MESSAGE, "paris paris");
-	alarm (2);
+	signal(SIGALRM, catchint);
+	result = netkeyer(K_MESSAGE, "paris paris");
+	alarm(2);
 	while (1) {}
 
 	printf("done");
 
-/* end tests */
-	result = netkeyer_close ();
-	if (result == 1) exit (1);
-	exit (0);
+	/* end tests */
+	if (netkeyer_close() == -1) {
+		exit(EXIT_FAILURE);
+	} else {
+		exit(EXIT_SUCCESS);
+	}
 }
