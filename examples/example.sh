51f7d64dba012b8d41218193b972594dc0c9e11d
acerion <acerion@wp.pl>
2023-05-13 15:02:46 +0200

    Tests: adding comment after initial tests on Alpine Linux

--- examples/example.sh
+++ examples/example.sh
@@ -6,6 +6,8 @@ trap 'stty "$old_tty_settings"; exit 0' INT
 
 echo "Press (CTRL-C) to interrupt..."
 
+# On Alpine Linux 1.17 running nc with 'localhost' name doesn't work.
+# 127.0.0.1 address must be used instead.
 while true; do
   nc -u localhost 6789
 done
