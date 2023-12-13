2329a737fab521ccf9b50d956ee95dd1982ac546
acerion <acerion@wp.pl>
2023-12-13 20:07:07 +0100

    Removing some checks from .clang-tidy's exclusions list
    The checks don't trigger any warnings during static code analysis. Removing
    them from the list of exclusions in this commit means that the checks will be
    performed during run of the lint tool.
    
    Also minor update in two files' comments.

--- tests/library/cw_rec_utils.c
+++ tests/library/cw_rec_utils.c
@@ -1,1 +1,1 @@
-#error "This file is not available in cwdaemon, copy cw_rec_utils.c from unixcw git repo."
+#error "This file is not available in cwdaemon, copy cw_rec_utils.c from unixcw git repo (non_recursive_make branch)."
