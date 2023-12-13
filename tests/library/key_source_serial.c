212c6cd6ae9ec847489c146d82ab5c5c1c131df8
acerion <acerion@wp.pl>
2023-12-13 20:15:29 +0100

    clang-tidy: fixing google-readability-todo warning

--- tests/library/key_source_serial.c
+++ tests/library/key_source_serial.c
@@ -24,7 +24,7 @@
 
 
 
-// TODO: GNU's strerror_r() may not fill provided buffer :(
+// TODO (acerion) 2022.12.17: GNU's strerror_r() may not fill provided buffer :(
 // Write own replacement of strerror_r() that bevaves as expected.
 //
 // I don't want to be guessing which version of strerror_r() is being
