e33f61bb0e87c5fec5206a5560e779b4aa8b1900
acerion <acerion@wp.pl>
2024-01-03 20:38:34 +0100

    Functional tests: removing unused code
    After all functional tests were refactored to use new APIs from tests
    'library, the old library code can be removed.
    
    Fewer global variables, yay!

--- tests/library/misc.h
+++ tests/library/misc.h
@@ -20,22 +20,6 @@ typedef struct {
 
 
 /**
-   @brief Ask cwdaemon to play a text message, receive it on serial line console
-
-   @param[in] client Client used to send and receive data to/from cwdaemon server
-   @param[in] messsage_value text to played
-   @param[in] expected_failed_receive whether a receiver is expected to receive the message incorrectly (i.e. not receive it at all)
-
-   @return 0 if text was received successfully
-   @return -1 otherwise
-*/
-int client_send_and_receive(client_t * client, const char * message_value, bool expected_failed_receive);
-int client_send_and_receive_2(client_t * client, cw_easy_receiver_t * morse_receiver, const char * message_value, bool expected_failed_receive);
-
-
-
-
-/**
    @brief Find a UDP port that is not used on local machine
 
    The port is randomly selected from range of ports above 1023 (above last
@@ -45,30 +29,6 @@ int client_send_and_receive_2(client_t * client, cw_easy_receiver_t * morse_rece
    @return 0 on failure to find an unused port
 */
 int find_unused_random_local_udp_port(void);
-
-
-
-
-/**
-   Configure some objects needed to run a test:
-    - libcw receiver (+ libcw generator needed by receiver),
-    - cwdevice observer (to monitor serial line)
-
-   @param[in] opts options for test helpers
-   @param[in] key_source_params parameters for cwdevice observer
-
-   @return 0 if setup was successful,
-   @return -1 otherwise
-*/
-int test_helpers_setup(const helpers_opts_t * opts, const cwdevice_observer_params_t * key_source_params);
-
-
-
-
-/**
-   Deconfigure objects configured with test_helpers_setup()
-*/
-void test_helpers_cleanup(void);
 
 
 
