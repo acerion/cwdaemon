bae912a318c39849b536836ee2c09b8816316008
acerion <acerion@wp.pl>
2024-04-20 11:10:31 +0200

    Tests: review: reviewing "test options" module from tests' library

--- tests/library/test_options.c
+++ tests/library/test_options.c
@@ -28,11 +28,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h> // strcasecmp()
 
 #include "libcw.h"
 #include "log.h"
 #include "supervisor.h"
 #include "test_options.h"
+
+
+
+
+/// @file
+///
+/// Handling of command line options passed to test programs and of options
+/// stored in dedicated ENV variables.
 
 
 
@@ -79,6 +88,16 @@ int test_options_get(int argc, char * const * argv, test_options_t * test_opts)
 
 
 
+/// @brief Parse shell env variables relevant to cwdaemon's test programs
+///
+/// Result of parsing is stored in @p test_opts
+///
+/// @reviewed_on{2024.04.19}
+///
+/// @param[out] test_opts Test options variable to fill
+///
+/// @return 0 on success (0 is also returned if env variables were not set)
+/// @return -1 on failure
 static int get_options_from_env(test_options_t * test_opts)
 {
 	char * value = NULL;
@@ -105,13 +124,33 @@ static int get_options_from_env(test_options_t * test_opts)
 
 
 
+/// @brief Parse command line options passed to cwdaemon's test programs
+///
+/// Result of parsing is stored in @p test_opts.
+///
+/// When "-h"/"--help" option is detected, function stops parsing the
+/// remainder of options. Function then sets test_opts::invoked_help to true,
+/// and returns 0.
+///
+/// @reviewed_on{2024.04.19}
+///
+/// @param[in] argc Test program's arguments count (main()'s "argc" argument)
+/// @param[in] argv Test program's arguments vector (main*)'s "argv" argument)
+/// @param[out] test_opts Test options variable to fill
+///
+/// @return 0 on success (0 is also returned if no options were passed)
+/// @return -1 on failure
 static int get_options_from_args(int argc, char * const * argv, test_options_t * test_opts)
 {
 	int option_index = 0;
 	int c = 0;
 	char * endptr = NULL;
 
-	while ((c = getopt_long(argc, argv, "h", g_test_options_long, &option_index)) != -1) {
+	// In general we disallow short options, but for "help" we make an
+	// exception.
+	char const * short_options = "h";
+
+	while ((c = getopt_long(argc, argv, short_options, g_test_options_long, &option_index)) != -1) {
 		switch (c) {
 		case 'x':
 			if (0 != get_sound_system(optarg, &test_opts->sound_system)) {
@@ -149,19 +188,28 @@ static int get_options_from_args(int argc, char * const * argv, test_options_t *
 
 
 
+/// @brief Parse given string as name of one of sound systems
+///
+/// @reviewed_on{2024.04.19}
+///
+/// @param[in] value String to be parsed
+/// @param[out] sound_system Sound system parsed from @p value
+///
+/// @return 0 on success
+/// @return -1 on failure
 static int get_sound_system(const char * value, enum cw_audio_systems * sound_system)
 {
-	if (0 == strcmp(value, "null")) {
+	if (0 == strcasecmp(value, "null")) {
 		*sound_system = CW_AUDIO_NULL;
-	} else if (0 == strcmp(value, "console")) {
+	} else if (0 == strcasecmp(value, "console")) {
 		*sound_system = CW_AUDIO_CONSOLE;
-	} else if (0 == strcmp(value, "oss")) {
+	} else if (0 == strcasecmp(value, "oss")) {
 		*sound_system = CW_AUDIO_OSS;
-	} else if (0 == strcmp(value, "alsa")) {
+	} else if (0 == strcasecmp(value, "alsa")) {
 		*sound_system = CW_AUDIO_ALSA;
-	} else if (0 == strcmp(value, "pulseaudio")) {
+	} else if (0 == strcasecmp(value, "pulseaudio")) {
 		*sound_system = CW_AUDIO_PA;
-	} else if (0 == strcmp(value, "soundcard")) {
+	} else if (0 == strcasecmp(value, "soundcard")) {
 		*sound_system = CW_AUDIO_SOUNDCARD;
 	} else {
 		test_log_err("Test options: failed at getting sound system from value string [%s]\n", value);
@@ -174,13 +222,22 @@ static int get_sound_system(const char * value, enum cw_audio_systems * sound_sy
 
 
 
+/// @brief Parse given string as name of one of supervisor programs
+///
+/// @reviewed_on{2024.04.19}
+///
+/// @param[in] value String to be parsed
+/// @param[out] supervisor_id ID of supervisor parsed from @p value
+///
+/// @return 0 on success
+/// @return -1 on failure
 static int get_supervisor_id(const char * value, supervisor_id_t * supervisor_id)
 {
-	if (0 == strcmp(value, "none")) {
+	if (0 == strcasecmp(value, "none")) {
 		*supervisor_id = supervisor_id_none;
-	} else if (0 == strcmp(value, "valgrind")) {
+	} else if (0 == strcasecmp(value, "valgrind")) {
 		*supervisor_id = supervisor_id_valgrind;
-	} else if (0 == strcmp(value, "gdb")) {
+	} else if (0 == strcasecmp(value, "gdb")) {
 		*supervisor_id = supervisor_id_gdb;
 	} else {
 		test_log_err("Test options: failed at getting supervisor id from value string [%s]\n", value);
@@ -193,8 +250,14 @@ static int get_supervisor_id(const char * value, supervisor_id_t * supervisor_id
 
 
 
+/// @brief Print test program's help text to given @p file
+///
+/// @reviewed_on{2024.04.19}
+///
+/// @param file File to which the help text will be printed
 static void print_help(FILE * file)
 {
+	//               10         20       30        40        50        60        70
 	/*       1234567890123456789012345678901234567890123456789012345678901234567890123456 */
 	fprintf(file,
 	        "This test program can be controlled by environment variables\n"
--- tests/library/test_options.h
+++ tests/library/test_options.h
@@ -1,5 +1,5 @@
-#ifndef CWDAEMON_TEST_LIB_TEST_OPTIONS_H
-#define CWDAEMON_TEST_LIB_TEST_OPTIONS_H
+#ifndef CWDAEMON_TESTS_LIB_TEST_OPTIONS_H
+#define CWDAEMON_TESTS_LIB_TEST_OPTIONS_H
 
 
 
@@ -15,19 +15,53 @@
 
 
 typedef struct test_options_t {
+	/// @brief Sound system to be used by cwdaemon server.
+	///
+	/// Used when starting local instance of tested server.
 	enum cw_audio_systems sound_system;
+
+	/// @brief Seed for random number generator used by test programs.
+	///
+	/// Used to initialized the random number generator.
 	uint32_t random_seed;
+
+	/// @brief Type of program supervising a cwdaemon server.
+	///
+	/// Used for supervising local instance of tested cwdaemon server.
 	supervisor_id_t supervisor_id;
+
+	/// @brief Show help text of test program?
 	bool invoked_help;
 } test_options_t;
 
 
 
 
+/// @brief Fill given @p test_opts variable
+///
+/// The @p test_opts variable is filled with values found in:
+/// 1. shell env variables dedicated to cwdaemon tests,
+/// 2. command line options passed to test program
+///
+/// Values from env variables are read first (if they are set at all). Then
+/// the command line options are parsed, and values of the options may
+/// overwrite values coming from env variables.
+///
+/// If "-h/--help" command line option was passed to program, this function
+/// sets test_opts::invoked_help to true. In that case a help text was show
+/// on console, and the test program program should exit with success.
+///
+/// @reviewed_on{2024.04.19}
+///
+/// @param[in] argc Test program's arguments count (main's "argc" argument)
+/// @param[in] argv Test program's arguments vector (main's "argv" argument)
+///
+/// @return 0 on successful parsing of env variables and/or options (even if there were no variables and/or options)
+/// @return -1 on failure
 int test_options_get(int argc, char * const * argv, test_options_t * test_opts);
 
 
 
 
-#endif /* #ifndef CWDAEMON_TEST_LIB_TEST_OPTIONS_H */
+#endif /* #ifndef CWDAEMON_TESTS_LIB_TEST_OPTIONS_H */
 
