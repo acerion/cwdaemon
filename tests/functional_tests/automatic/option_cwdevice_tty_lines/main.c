055fda1131585d3713be746770df4705b79189ff
acerion <acerion@wp.pl>
2024-05-05 14:07:28 +0200

    Tests: refactoring "tty lines option" test some more
    Making sure that conventions and solutions applied recently to other tests
    are also applied to this test.
    
    The test was reviewed not so long ago, but since then I learned how to do
    some things better.

--- tests/functional_tests/automatic/option_cwdevice_tty_lines/main.c
+++ tests/functional_tests/automatic/option_cwdevice_tty_lines/main.c
@@ -63,17 +63,17 @@
 
 
 typedef struct test_case_t {
-	const char * description;                /**< Tester-friendly description of test case. */
+	char const * description;                /**< Tester-friendly description of test case. */
 	tty_pins_t server_tty_pins;              /**< Configuration of tty pins on cwdevice used by cwdaemon server. */
 	tty_pins_t observer_tty_pins;            /**< Which tty pins on cwdevice should be treated by cwdevice as keying or ptt pins. */
-	test_request_t full_message;             /**< Text to be sent to cwdaemon server in the MESSAGE request. */
-	event_t expected_events[EVENTS_MAX];     /**< Events that we expect to happen in this test case. */
+	test_request_t plain_request;            /**< Text to be sent to cwdaemon server in the MESSAGE request. */
+	event_t expected[EVENTS_MAX];     /**< Events that we expect to happen in this test case. */
 } test_case_t;
 
 
 
 
-/// @reviewed_on{2024.04.28}
+/// @reviewed_on{2024.05.05}
 static test_case_t g_test_cases[] = {
 	// This is a SUCCESS case.
 	//
@@ -82,9 +82,9 @@ static test_case_t g_test_cases[] = {
 	//
 	// Pins for cwdevice observer are not configured explicitly. The observer
 	// uses implicit default configuration of pins.
-	{ .description             = "success case, setup without tty line options passed to cwdaemon",
-	  .full_message            =                             TESTS_SET_BYTES("madrit"),
-	  .expected_events         = { { .etype = etype_morse, .u.morse.string = "madrit"  }, },
+	{ .description = "success case, setup without tty line options passed to cwdaemon",
+	  .plain_request =                                 TESTS_SET_BYTES("madrit"),
+	  .expected = { { .etype = etype_morse, .u.morse = TESTS_SET_MORSE("madrit")  }, },
 	},
 
 	// This is a SUCCESS case.
@@ -95,10 +95,10 @@ static test_case_t g_test_cases[] = {
 	//
 	// Pins for cwdevice observer are not configured explicitly. The observer
 	// uses implicit default configuration of pins.
-	{ .description             = "success case, setup with explicitly setting default tty lines options passed to cwdaemon",
-	  .server_tty_pins         = { .explicit = true, .pin_keying = TIOCM_DTR, .pin_ptt = TIOCM_RTS },
-	  .full_message            =                             TESTS_SET_BYTES("lisbon"),
-	  .expected_events         = { { .etype = etype_morse, .u.morse.string = "lisbon" }, },
+	{ .description = "success case, setup with explicitly setting default tty lines options passed to cwdaemon",
+	  .server_tty_pins = { .explicit = true, .pin_keying = TIOCM_DTR, .pin_ptt = TIOCM_RTS },
+	  .plain_request =                                 TESTS_SET_BYTES("lisbon"),
+	  .expected = { { .etype = etype_morse, .u.morse = TESTS_SET_MORSE("lisbon") }, },
 	},
 
 	// This is a FAILURE case.
@@ -112,11 +112,11 @@ static test_case_t g_test_cases[] = {
 	//
 	// Since cwdaemon and cwdevice observer have different configuration of
 	// pins, the receive process will fail.
-	{ .description             = "failure case, cwdaemon is keying DTR, cwdevice observer is monitoring RTS",
-	  .server_tty_pins         = { .explicit = true, .pin_keying = TIOCM_DTR, .pin_ptt = TIOCM_RTS },
-	  .observer_tty_pins       = { .explicit = true, .pin_keying = TIOCM_RTS, .pin_ptt = TIOCM_DTR },
-	  .full_message            = TESTS_SET_BYTES("paris"),
-	  .expected_events         = { { 0 } },
+	{ .description = "failure case, cwdaemon is keying DTR, cwdevice observer is monitoring RTS",
+	  .server_tty_pins   = { .explicit = true, .pin_keying = TIOCM_DTR, .pin_ptt = TIOCM_RTS },
+	  .observer_tty_pins = { .explicit = true, .pin_keying = TIOCM_RTS, .pin_ptt = TIOCM_DTR },
+	  .plain_request = TESTS_SET_BYTES("paris"),
+	  .expected      = { { 0 } },
 	},
 
 	// This is a SUCCESS case.
@@ -130,13 +130,18 @@ static test_case_t g_test_cases[] = {
 	//
 	// Since cwdaemon and cwdevice observer have the same configuration of
 	// pins, the receive process will succeed.
-	{ .description             = "success case, cwdaemon is keying RTS, cwdevice observer is monitoring RTS",
-	  .server_tty_pins         = { .explicit = true, .pin_keying = TIOCM_RTS, .pin_ptt = TIOCM_DTR },
-	  .observer_tty_pins       = { .explicit = true, .pin_keying = TIOCM_RTS, .pin_ptt = TIOCM_DTR },
-	  .full_message            =                             TESTS_SET_BYTES("dublin"),
-	  .expected_events         = { { .etype = etype_morse, .u.morse.string = "dublin" }, },
+	{ .description = "success case, cwdaemon is keying RTS, cwdevice observer is monitoring RTS",
+	  .server_tty_pins   = { .explicit = true, .pin_keying = TIOCM_RTS, .pin_ptt = TIOCM_DTR },
+	  .observer_tty_pins = { .explicit = true, .pin_keying = TIOCM_RTS, .pin_ptt = TIOCM_DTR },
+	  .plain_request =                                 TESTS_SET_BYTES("dublin"),
+	  .expected = { { .etype = etype_morse, .u.morse = TESTS_SET_MORSE("dublin") }, },
 	},
 };
+
+
+
+
+static char const * g_test_name = "option - tty lines";
 
 
 
@@ -144,22 +149,22 @@ static test_case_t g_test_cases[] = {
 static int testcase_setup(server_t * server, client_t * client, morse_receiver_t * morse_receiver, const test_case_t * test_case, test_options_t * test_opts);
 static int testcase_run(const test_case_t * test_case, client_t * client, morse_receiver_t * morse_receiver);
 static int testcase_teardown(server_t * server, client_t * client, morse_receiver_t * morse_receiver);
-static int evaluate_events(events_t * events, const test_case_t * test_case);
+static int evaluate_events(events_t const * recorded_events, test_case_t const * test_case);
 
 
 
 
-/// @reviewed_on{2024.04.28}
+/// @reviewed_on{2024.04.05}
 int main(int argc, char * const * argv)
 {
 	if (!testing_env_is_usable(testing_env_libcw_without_signals)) {
-		test_log_err("Test: preconditions for testing env are not met, exiting %s\n", "");
+		test_log_err("Test: preconditions for testing env are not met, exiting test [%s]\n", g_test_name);
 		exit(EXIT_FAILURE);
 	}
 
 	test_options_t test_opts = { .sound_system = CW_AUDIO_SOUNDCARD };
 	if (0 != test_options_get(argc, argv, &test_opts)) {
-		test_log_err("Test: failed to process env variables and command line options %s\n", "");
+		test_log_err("Test: failed to process env variables and command line options for test [%s]\n", g_test_name);
 		exit(EXIT_FAILURE);
 	}
 	if (test_opts.invoked_help) {
@@ -170,7 +175,7 @@ int main(int argc, char * const * argv)
 	const uint32_t seed = cwdaemon_srandom(test_opts.random_seed);
 	test_log_debug("Test: random seed: 0x%08x (%u)\n", seed, seed);
 
-
+	bool overall_test_failure = false;
 	const size_t n_test_cases = sizeof (g_test_cases) / sizeof (g_test_cases[0]);
 	for (size_t i = 0; i < n_test_cases; i++) {
 		const test_case_t * test_case = &g_test_cases[i];
@@ -196,6 +201,7 @@ int main(int argc, char * const * argv)
 			goto cleanup;
 		}
 
+		events_sort(&events);
 		if (0 != evaluate_events(&events, test_case)) {
 			test_log_err("Test: evaluation of events has failed for test case %zu / %zu\n", i + 1, n_test_cases);
 			failure = true;
@@ -210,15 +216,19 @@ int main(int argc, char * const * argv)
 
 		if (failure) {
 			test_log_err("Test: test case #%zu/%zu failed, terminating\n", i + 1, n_test_cases);
-			test_log_newline(); /* Visual separator. */
-			test_log_err("Test: FAIL %s\n", "");
-			exit(EXIT_FAILURE);
+			overall_test_failure = true;
+			break;
 		}
 		test_log_info("Test: test case #%zu/%zu succeeded\n\n", i + 1, n_test_cases);
 	}
 
+	if (overall_test_failure) {
+		test_log_newline(); /* Visual separator. */
+		test_log_err("Test: FAIL ([%s] test)\n", g_test_name);
+		exit(EXIT_FAILURE);
+	}
 	test_log_newline(); /* Visual separator. */
-	test_log_info("Test: PASS %s\n", "");
+	test_log_info("Test: PASS ([%s] test)\n", g_test_name);
 	exit(EXIT_SUCCESS);
 }
 
@@ -230,8 +240,6 @@ int main(int argc, char * const * argv)
 /// @reviewed_on{2024.04.28}
 static int testcase_setup(server_t * server, client_t * client, morse_receiver_t * morse_receiver, const test_case_t * test_case, test_options_t * test_opts)
 {
-	bool failure = false;
-
 	const int wpm = tests_get_test_wpm();
 
 	/* Prepare local test instance of cwdaemon server. */
@@ -245,13 +253,13 @@ static int testcase_setup(server_t * server, client_t * client, morse_receiver_t
 	};
 	if (0 != server_start(&server_opts, server)) {
 		test_log_err("Test: failed to start cwdaemon server %s\n", "");
-		failure = true;
+		return -1;
 	}
 
 
 	if (0 != client_connect_to_server(client, server->ip_address, (in_port_t) server->l4_port)) { /* TODO acerion 2024.01.29: remove casting. */
 		test_log_err("Test: can't connect cwdaemon client to cwdaemon server at [%s:%d]\n", server->ip_address, server->l4_port);
-		failure = true;
+		return -1;
 	}
 
 
@@ -259,10 +267,10 @@ static int testcase_setup(server_t * server, client_t * client, morse_receiver_t
 	                                               .wpm = wpm };
 	if (0 != morse_receiver_configure(&morse_config, morse_receiver)) {
 		test_log_err("Test: failed to configure Morse receiver %s\n", "");
-		failure = true;
+		return -1;
 	}
 
-	return failure ? -1 : 0;
+	return 0;
 }
 
 
@@ -272,7 +280,7 @@ static int testcase_setup(server_t * server, client_t * client, morse_receiver_t
    @brief Run single test case. Evaluate its results (the events).
 
    cwdaemon server will be playing message from testcase
-   (test_case->full_message) and will be keying a specific line of tty device
+   (test_case->plain_request) and will be keying a specific line of tty device
    (test_case->cwdaemon_param_keying).
 
    The cwdevice observer will be observing a tty line that it was told to
@@ -291,7 +299,7 @@ static int testcase_run(const test_case_t * test_case, client_t * client, morse_
 		return -1;
 	}
 
-	client_send_request(client, &test_case->full_message);
+	client_send_request(client, &test_case->plain_request);
 
 	morse_receiver_wait_for_stop(morse_receiver);
 
@@ -344,60 +352,26 @@ static int testcase_teardown(server_t * server, client_t * client, morse_receive
      - changes of state of PTT pin,
      - exiting of local instance of cwdaemon server process,
 
-   @reviewed_on{2024.04.28}
+   @reviewed_on{2024.05.05}
 */
-static int evaluate_events(events_t * events, const test_case_t * test_case)
+static int evaluate_events(events_t const * recorded_events, test_case_t const * test_case)
 {
-	events_sort(events);
-	events_print(events); // This is mostly for debugging.
-
-	int expectation_idx = 0; /* To recognize failing expectations more easily. */
-	event_t const * expected_events = test_case->expected_events;
+	events_print(recorded_events); // For debug only.
 
 
+	int expectation_idx = 0; // To recognize failing expectations more easily.
+	event_t const * const expected = test_case->expected;
+	event_t const * const recorded = recorded_events->events;
 
 
-	/* Expectation: correct count of events. */
+	// Expectation: correct count, types, order and contents of events.
 	expectation_idx = 1;
-	const int expected_events_cnt = events_get_count(expected_events);
-	if (0 != expect_count_of_events(expectation_idx, events->events_cnt, expected_events_cnt)) {
+	if (0 != expect_count_type_order_contents(expectation_idx, expected, recorded)) {
 		return -1;
 	}
 
 
-
-
-	/* Expectation: correct types, order and contents of events. */
-	expectation_idx = 2;
-	for (int i = 0; i < events->events_cnt; i++) {
-		if (expected_events[i].etype != events->events[i].etype) {
-			test_log_err("Expectation %d: unexpected event %u at position %d\n", expectation_idx, events->events[i].etype, i);
-			return -1;
-		}
-
-		switch (events->events[i].etype) {
-		case etype_morse:
-			if (0 != expect_morse_match(expectation_idx,
-			                            &events->events[i].u.morse,
-			                            expected_events[i].u.morse.string)) {
-				return -1;
-			}
-			break;
-		case etype_none:
-		case etype_reply:
-		case etype_req_exit:
-		case etype_sigchld:
-		default:
-			test_log_err("Expectation %d: unhandled event type %u at position %d\n", expectation_idx, events->events[i].etype, i);
-			return -1;
-		}
-	}
-	test_log_info("Expectation %d: found expected types of events, in proper order\n", expectation_idx);
-
-
-
-
-	test_log_info("Test: evaluation of test events was successful %s\n", "");
+	test_log_info("Test: evaluation of test events was successful for test case [%s]\n", test_case->description);
 
 	return 0;
 }
