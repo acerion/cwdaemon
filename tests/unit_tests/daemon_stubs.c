84fae44f345d1608d251252a29399d4057cb10f7
acerion <acerion@wp.pl>
2024-05-12 17:15:51 +0200

    Tests: preparing unit tests programs for modification of cwdaemon's options.c file
    One of files from cwdaemon that is also compiled in unit tests code will soon
    require a stub in the unit tests. This commit is adding the stub function to
    the unit tests. Next commit will make the stub necessary.

---
+++ tests/unit_tests/daemon_stubs.c
@@ -0,0 +1,50 @@
+/*
+ * This file is a part of cwdaemon project.
+ *
+ * Copyright (C) 2002 - 2005 Joop Stakenborg <pg4i@amsat.org>
+ *		        and many authors, see the AUTHORS file.
+ * Copyright (C) 2012 - 2024 Kamil Ignacak <acerion@wp.pl>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+
+
+
+/// @file
+///
+/// Stubs for functions that are used in daemon's files compiled with unit
+/// tests.
+
+
+
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "src/cwdaemon.h"
+
+
+
+
+// @reviewed_on{2024.05.12}
+bool cwdaemon_cwdevice_set(__attribute__((unused)) cwdevice **device, __attribute__((unused)) const char * desc)
+{
+	return true;
+}
+
+
